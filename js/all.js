30cf67404f6173a68a5b5fffa904d2b7ad30a2fc
Pickachu Chen <pickachu1006@gmail.com>
2021-03-18 13:05:37 +0800

    20210318_init

---
+++ js/all.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    // 立即訂購btn動態
+    $('.buybtn').click(function (e) {
+        e.preventDefault();
+        $('html , body').animate({
+            scrollTop: $('#buy').offset().top,
+        }, 900);
+    });
+
+    // $("#wh-call-to-action").removeAttr("wh-click");
+    $("#wh-call-to-action").removeClass("wh-animation-in");
+    //聊天機器人
+    $(window).scroll(function () {
+        var scrollPos = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var formTop = $('#lp_form').position().top;
+        var width = $(window).width();
+
+        if ((formTop <= (scrollPos + windowHeight / 2)) && (width <= 768)) {
+            $("#fb-root").attr({
+                style: "display: none;"
+            });
+            $("#wrapper img#line").attr({
+                style: "display: block;"
+            });
+            $(".line_alert").attr({
+                style: "display: block;"
+            });
+        } else if ((formTop <= (scrollPos + windowHeight / 2)) && (width > 768)) {
+            $("#fb-root").attr({
+                style: "display: block;"
+            });
+        } 
+    });
+
+     //週年慶活動辦法下拉選單
+    //  $(".anniversary_2").hide();
+    //  $('.anniversary_1').click(function (e) { 
+    //      e.preventDefault();
+    //      $(".anniversary_2").toggle();
+    //  });
+});
